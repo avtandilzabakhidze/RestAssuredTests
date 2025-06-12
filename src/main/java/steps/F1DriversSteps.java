99dd4276f105e9a557951b12394697b928b6561c
avtandilzabakhidze <zabakhidze01@gmail.com>
2025-06-12 16:49:21 +0400

    add step class for F1DriversTest

---
+++ src/main/java/steps/F1DriversSteps.java
@@ -0,0 +1,92 @@
+package ge.tbc.tbcitacademy.Steps.F1Steps;
+
+import enums.Countries;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import java.util.List;
+
+import static data.Constants.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class F1DriversSteps {
+    private Response response;
+
+    public F1DriversSteps setBaseUri() {
+        RestAssured.baseURI = F1_BASE_URI;
+        return this;
+    }
+
+    public F1DriversSteps sendGetRequest() {
+        response = given()
+                .when()
+                .get(F1_BASE_URI + F1_2025_DRIVERS_ENDPOINT);
+        return this;
+    }
+
+    public F1DriversSteps validateStatusAndMetadata() {
+        response.then()
+                .statusCode(200)
+                .body("MRData.series", equalTo(SERIES_F1))
+                .body("MRData.DriverTable.season", equalTo(SEASON_2025));
+        return this;
+    }
+
+    public F1DriversSteps validateDriverCount() {
+        int actualSize = response.path("MRData.DriverTable.Drivers.size()");
+        int total = Integer.parseInt(response.path("MRData.total"));
+        assertThat(actualSize, equalTo(total));
+        return this;
+    }
+
+    public F1DriversSteps validateOldDriver() {
+        String driverName = response.path("MRData.DriverTable.Drivers.find { it.dateOfBirth < '1990-01-01' }.givenName") +
+                " " + response.path("MRData.DriverTable.Drivers.find { it.dateOfBirth < '1990-01-01' }.familyName");
+        assertThat(DRIVER_NAME_NOT_EMPTY, driverName, not(emptyString()));
+        return this;
+    }
+
+    public F1DriversSteps validatePost2000Drivers() {
+        List<String> post2000 = response.path("MRData.DriverTable.Drivers.findAll { it.dateOfBirth > '2000-01-01' }.collect { it.givenName + ' ' + it.familyName }");
+        assertThat(POST_2000_MIN_COUNT_MSG, post2000.size(), greaterThanOrEqualTo(EIGHT));
+        return this;
+    }
+
+    public F1DriversSteps validateFrenchDriversCount() {
+        int frenchCount = response.path("MRData.DriverTable.Drivers.findAll { it.nationality == 'French' }.size()");
+        assertThat(FRENCH_DRIVERS_EXPECTED, frenchCount, equalTo(THREE));
+        return this;
+    }
+
+    public F1DriversSteps validateDriversStartingWithAB() {
+        int abCount = response.path("MRData.DriverTable.Drivers.findAll { it.familyName ==~ /^[ABab].*/ }.size()");
+        assertThat(AB_NAME_START_MIN, abCount, greaterThanOrEqualTo(FIVE));
+        return this;
+    }
+
+    public F1DriversSteps validateYoungBritishDrivers() {
+        List<String> british = response.path("MRData.DriverTable.Drivers.findAll { it.nationality == 'British' && it.dateOfBirth > '1990-01-01' }.collect { it.givenName + ' ' + it.familyName }");
+        assertThat(BRITISH_YOUNG_MIN, british.size(), greaterThanOrEqualTo(THREE));
+        return this;
+    }
+
+    public F1DriversSteps validateSpecialDrivers() {
+        List<String> specialDrivers = response.path("MRData.DriverTable.Drivers.findAll { it.permanentNumber?.toInteger() < 10 || it.familyName.length() > 7 }.collect { it.givenName + ' ' + it.familyName }");
+        System.out.println(specialDrivers);
+        assertThat(SPECIAL_DRIVERS_MIN, specialDrivers.size(), greaterThanOrEqualTo(FIVE));
+        return this;
+    }
+
+    public F1DriversSteps validateNationalityChecks() {
+        assertThat(findDriversByNationality(Countries.DUTCH.getValue()), hasItem(containsString(EXPECTED_DUTCH_DRIVER)));
+        assertThat(findDriversByNationality(Countries.BRAZILIAN.getValue()).size(), greaterThanOrEqualTo(FIRST));
+        assertThat(findDriversByNationality(Countries.CANADIAN.getValue()), hasItem(EXPECTED_CANADIAN_DRIVER));
+        return this;
+    }
+
+    private List<String> findDriversByNationality(String nationality) {
+        return response.path("MRData.DriverTable.Drivers.findAll { it.nationality == '" + nationality + "' }.collect { it.givenName + ' ' + it.familyName }");
+    }
+}
--- src/test/java/F1DriversTest.java
+++ src/test/java/F1DriversTest.java
@@ -1,60 +1,28 @@
-import enums.Countries;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
+import ge.tbc.tbcitacademy.Steps.F1Steps.F1DriversSteps;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
-import static data.Constants.*;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-
 public class F1DriversTest {
-    @Test(priority = 1)
-    public void validateF1Drivers() {
-        RestAssured.baseURI = F1_BASE_URI;
+    private F1DriversSteps f1DriversSteps;
 
-        Response response = given()
-                .when()
-                .get(F1_BASE_URI + F1_2025_DRIVERS_ENDPOINT);
-
-        response.then()
-                .statusCode(200)
-                .body("MRData.series", equalTo(SERIES_F1))
-                .body("MRData.DriverTable.season", equalTo(SEASON_2025));
-
-        int actualSize = response.path("MRData.DriverTable.Drivers.size()");
-        int total = Integer.parseInt(response.path("MRData.total"));
-        assertThat(actualSize, equalTo(total));
-
-        String driverName = response.path("MRData.DriverTable.Drivers.find { it.dateOfBirth < '1990-01-01' }.givenName") +
-                " " + response.path("MRData.DriverTable.Drivers.find { it.dateOfBirth < '1990-01-01' }.familyName");
-        assertThat(DRIVER_NAME_NOT_EMPTY, driverName, not(emptyString()));
-
-        List<String> post2000 = response.path("MRData.DriverTable.Drivers.findAll { it.dateOfBirth > '2000-01-01' }.collect { it.givenName + ' ' + it.familyName }");
-        assertThat(POST_2000_MIN_COUNT_MSG, post2000.size(), greaterThanOrEqualTo(EIGHT));
-
-        int frenchCount = response.path("MRData.DriverTable.Drivers.findAll { it.nationality == 'French' }.size()");
-        assertThat(FRENCH_DRIVERS_EXPECTED, frenchCount, equalTo(THREE));
-
-        int abCount = response.path("MRData.DriverTable.Drivers.findAll { it.familyName ==~ /^[ABab].*/ }.size()");
-        assertThat(AB_NAME_START_MIN, abCount, greaterThanOrEqualTo(FIVE));
-
-        List<String> british = response.path("MRData.DriverTable.Drivers.findAll { it.nationality == 'British' && it.dateOfBirth > '1990-01-01' }.collect { it.givenName + ' ' + it.familyName }");
-        assertThat(BRITISH_YOUNG_MIN, british.size(), greaterThanOrEqualTo(THREE));
-
-        List<String> specialDrivers = response.path("MRData.DriverTable.Drivers.findAll { it.permanentNumber?.toInteger() < 10 || it.familyName.length() > 7 }.collect { it.givenName + ' ' + it.familyName }");
-        System.out.println(specialDrivers);
-
-        assertThat(SPECIAL_DRIVERS_MIN, specialDrivers.size(), greaterThanOrEqualTo(FIVE));
-
-        assertThat(findDriversByNationality(Countries.DUTCH.getValue(), response), hasItem(containsString(EXPECTED_DUTCH_DRIVER)));
-        assertThat(findDriversByNationality(Countries.BRAZILIAN.getValue(), response).size(), greaterThanOrEqualTo(FIRST));
-        assertThat(findDriversByNationality(Countries.CANADIAN.getValue(), response), hasItem(EXPECTED_CANADIAN_DRIVER));
+    @BeforeMethod
+    public void setUp() {
+        f1DriversSteps = new F1DriversSteps();
     }
 
-    public List<String> findDriversByNationality(String nationality, Response response) {
-        return response.path("MRData.DriverTable.Drivers.findAll { it.nationality == '" + nationality + "' }.collect { it.givenName + ' ' + it.familyName }");
+    @Test(priority = 1)
+    public void validateF1Drivers() {
+        new F1DriversSteps()
+                .setBaseUri()
+                .sendGetRequest()
+                .validateStatusAndMetadata()
+                .validateDriverCount()
+                .validateOldDriver()
+                .validatePost2000Drivers()
+                .validateFrenchDriversCount()
+                .validateDriversStartingWithAB()
+                .validateYoungBritishDrivers()
+                .validateSpecialDrivers()
+                .validateNationalityChecks();
     }
 }
